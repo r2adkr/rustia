334ef4a756ec94d177dd5596f5095a4fefddd270
r2adkr <kyoomin0511@gmail.com>
2026-03-03 20:41:00 +0900

    feat: Added a function to retrieve node information.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,39 @@
 #![deny(clippy::all)]
 mod types;
+use crate::types::nodetype::Node;
 use napi_derive::napi;
 
-use crate::types::nodetype::Node;
+#[napi(object)]
+pub struct RustiaOptions {
+  pub nodes: Vec<Node>,
+}
 
 #[napi]
 pub struct Rustia {
-  pub nodes: Vec<Node>,
+  nodes: Vec<Node>,
+}
+
+#[napi]
+impl Rustia {
+  /// Create a new Rustia instance.
+  /// ### Example
+  /// ```javascript
+  /// const options = {
+  ///   nodes: [
+  ///     { name: "my-node", host: "127.0.0.1:2333", secure: false }
+  ///   ]
+  /// };
+  /// const rustia = new Rustia(options);
+  /// ```
+  #[napi(constructor)]
+  pub fn new(options: RustiaOptions) -> Self {
+    Self {
+      nodes: options.nodes,
+    }
+  }
+  /// Returns information about registered nodes.
+  #[napi]
+  pub fn get_nodes(&self) -> Vec<Node> {
+    self.nodes.clone()
+  }
 }
--- src/types/nodetype.rs
+++ src/types/nodetype.rs
@@ -1,15 +1,17 @@
 use napi_derive::napi;
 
 /// It's a structure that manages node connections.
-/// ### Exapmle
+/// ### Example
 /// ```
 /// {
 ///     name: "node-name",
 ///     host: "myserver:2333",
 ///     secure: true,
+///     is_nodelink: true,
 ///     session_id: "session_id",
 /// }
 /// ```
+#[derive(Clone)]
 #[napi(object)]
 pub struct Node {
   /// Node name
@@ -18,6 +20,8 @@ pub struct Node {
   pub host: String,
   /// Whether the server uses a secure connection
   pub secure: bool,
+  /// If you are using NodeLink, set this option to true.
+  pub is_nodelink: Option<bool>,
   /// Option: Session ID
   pub session_id: Option<String>,
 }
